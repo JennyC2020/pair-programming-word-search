c4da04e3cd213950f5ab57e54857f9cbccf338aa
Jenny Citherlet <jennycith@gmail.com>
2020-07-30 02:23:48 +0000

    Inital commit test cases - Marisa Doig & Jenny Citherlet

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -35,4 +35,50 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'A', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'P', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'P', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'L', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'APPLE')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the matrix is empty", function() {
+    const result = wordSearch([
+      [],
+      [],
+      [],
+      [],
+      [],
+      []
+    ], 'APPLE')
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if no word", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'A', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'P', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'P', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'L', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '')
+
+    assert.isFalse(result);
+  });
+
 });
